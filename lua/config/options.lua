12ab886027cd9bad9c258edf54c2eec1492db4e2
Javier Furus <javierfurus@gmail.com>
2026-02-26 13:11:23 +0100

    use lazygit, update configs

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Set line numbbers to absolute
+vim.opt.relativenumber = false
+vim.diagnostic.config({
+  update_in_insert = true, -- Show diagnostics while typing in insert mode
+  virtual_text = true, -- Show inline error messages
+  float = { border = "rounded" },
+  severity_sort = true,
+})
